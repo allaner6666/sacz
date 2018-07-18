fd5fd3c690f5fd960f306f38bf06327ee353dd39
pohcbSonic <pohcb.hypersonic@gmail.com>
2018-07-18 16:15:08 +0800

    added in test_Mark_item method.

--- jtest/jtest1.java
+++ jtest/jtest1.java
@@ -17,10 +17,10 @@ public class jtest1 {
 	@Test
 	public void test_add_new_item() {
 		program mft1 = new program();
-		assertEquals(true, mft1.addNewItem("hello"));
+		assertTrue(mft1.addNewItem("hello"));
 		int count=mft1.showCurrentList();
 		assertTrue(count==1);
-		assertEquals(false, mft1.addNewItem("hello"));
+		assertFalse(mft1.addNewItem("hello"));
 		count=mft1.showCurrentList();
 		assertTrue(count==1);
 	}
@@ -38,5 +38,19 @@ public class jtest1 {
 		mft.deleteItem("item1");
 		ArrayList<String> testList2 = new ArrayList<String>();
 		assertTrue("result", testList2.equals(mft.list));
+	}
+	
+	@Test
+	public void test_Mark_item()
+	{
+		program mft = new program();
+		
+		mft.findITemInTheList("itemtoFind1");
+		ArrayList<String> testList3 = new ArrayList<String>();
+		assertTrue("result", testList3.equals(mft.list));
+		
+		mft.showItemAsComplete("itemComplete1");
+		ArrayList<String> testList4 = new ArrayList<String>();
+		assertTrue("result", testList4.equals(mft.list));
 	}
 }
